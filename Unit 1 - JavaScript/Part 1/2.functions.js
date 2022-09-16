77e57992410541161b0fc21e360a4b1ee093f2c0
Arturo Bernal <arturo@iessanvicente.com>
2022-09-16 09:55:55 +0200

    functions

--- Unit 1 - JavaScript/Part 1/2.functions.js
+++ Unit 1 - JavaScript/Part 1/2.functions.js
@@ -1,22 +1,22 @@
 "use strict";
 
 function sayHello() {
-    console.log("Hello World!");
+  console.log("Hello World!");
 }
 
 sayHello();
 
 function sayHello2(name) {
-    if(typeof name !== "string") {
-        console.error("Name must be a string!");
-        return;
-    }
+  if (typeof name !== "string") {
+    console.error("Name must be a string!");
+    return;
+  }
 
-    if (name) {
-        console.log("Hello " + name);
-    } else {
-        console.error("No name received!");
-    }
+  if (name) {
+    console.log("Hello " + name);
+  } else {
+    console.error("No name received!");
+  }
 }
 
 sayHello2("Peter"); // Hello Peter
@@ -25,4 +25,39 @@ sayHello2(""); // No name received!
 sayHello2(); // Name must be string! (undefined)
 sayHello2(23); // Name must be string!
 
+function add(n1, n2) {
+  return n1 + n2;
+}
 
+console.log(add(6, 7));
+
+let addAnonymous = function (n1, n2) {
+  return n1 + n2;
+};
+
+console.log(addAnonymous(6, 7));
+
+console.log(typeof add); // function
+console.log(typeof addAnonymous); // function
+
+let add2 = add;
+console.log(add2(4, 5));
+
+function calculator(n1, n2, op) {
+  console.log(op(n1, n2));
+}
+
+calculator(5, 8, add); // 13
+calculator(5, 8, addAnonymous); // 13
+calculator(5, 8, function (n1, n2) {
+  return n1 - n2;
+});
+calculator(5, 8, (n1, n2) => n1 * n2);
+
+function sayHello3(name = "Anonymous") {
+    console.log("Hello " + name);
+}
+
+sayHello3(); // Hello Anonymous
+sayHello3(""); // Hello Anonymous
+sayHello3("Mary"); // Hello Mary
