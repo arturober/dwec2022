2b361318689ffcb9a49d0e8365e6291ad4d509f9
Arturo Bernal <arturo@iessanvicente.com>
2022-10-06 09:05:36 +0200

    objects

---
+++ Unit 1 - JavaScript/Part 3/exercise objects/exercise objects.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// Create a class "Product" with name and price
+// Create an array with at least 4 products
+// Order that array by product's name and show it
+// Order that array by product's price and show it
+
+// a.sort((p1, p2) => );
