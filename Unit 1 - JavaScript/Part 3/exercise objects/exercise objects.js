f82c7058e1ccecb33fb2884089998f169a57636f
Arturo Bernal <arturo@iessanvicente.com>
2022-10-06 09:42:21 +0200

    exercise objects

--- Unit 1 - JavaScript/Part 3/exercise objects/exercise objects.js
+++ Unit 1 - JavaScript/Part 3/exercise objects/exercise objects.js
@@ -5,4 +5,26 @@
 // Order that array by product's name and show it
 // Order that array by product's price and show it
 
-// a.sort((p1, p2) => );
+class Product {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    toString() {
+        return `${this.name} (${this.price})`;
+    }
+}
+
+let products = [
+    new Product("chair", 45),
+    new Product("table", 100),
+    new Product("lamp", 15.95),
+    new Product("armchair", 67.5)
+];
+
+products.sort((p1, p2) => p1.name.localeCompare(p2.name));
+console.log(products.toString());
+
+products.sort((p1, p2) => p1.price - p2.price);
+console.log(products.toString());
