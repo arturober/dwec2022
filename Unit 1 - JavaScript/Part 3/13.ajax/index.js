6331bff0450be3c9be5193059db7328235c17c35
Arturo Bernal <arturo@iessanvicente.com>
2022-10-11 09:54:28 +0200

    ajax

--- Unit 1 - JavaScript/Part 3/13.ajax/index.js
+++ Unit 1 - JavaScript/Part 3/13.ajax/index.js
@@ -1,3 +1,28 @@
-fetch('http://arturober.com:5007/restaurants').then(
-    resp => resp.json()
-).then(json => console.log(json));
+fetch('http://arturober.com:5007/restaurants')
+.then(resp => resp.json())
+.then(json => console.log(json));
+
+/*
+fetch(`${SERVER}/restaurants`, {
+    method: 'POST',
+    body: JSON.stringify(restaurant),
+    headers: {
+    'Content-Type': 'application/json'
+    }
+}).then(resp => {
+    if(!resp.ok) throw new Error(resp.statusText);
+    return resp.json(); // promise
+}).then(respJSON => {
+    // Put restaurant in HTML
+}).catch(error => console.error(error));
+*/
+fetch('http://arturober.com:5007/restaurants', {
+    method: 'DELETE'
+})
+.then(resp => {
+    if(!resp.ok) throw new Error(resp.statusText);
+    // OK Delete restaurant in HTML
+});
+/*
+
+*/
