a5a62215822a24d7ffa6b273034d7c0cf80474a3
Arturo Bernal <arturo@iessanvicente.com>
2022-10-11 09:54:20 +0200

    ajax

---
+++ Unit 1 - JavaScript/Part 3/exercise ajax/index.js
@@ -0,0 +1,4 @@
+// Web services http://arturober.com:5006/tareas
+// GET all the todos and show them in the list
+// When sending the form, generate a POST call and insert a new task
+// OPTIONAL: Include a button to delete the task
