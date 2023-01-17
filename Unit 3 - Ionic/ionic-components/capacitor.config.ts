5ae92b3c2200d4a9948ad24e270f40aa9852ea71
Arturo Bernal <arturober@gmail.com>
2023-01-17 10:27:08 +0100

    ionic capacitor

--- Unit 3 - Ionic/ionic-components/capacitor.config.ts
+++ Unit 3 - Ionic/ionic-components/capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'ionic-components',
+  appId: 'com.arturober.components',
+  appName: 'Ionic Components',
   webDir: 'www',
   bundledWebRuntime: false
 };
